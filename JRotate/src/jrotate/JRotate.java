faa6309c59898be49e9028a00dfddb6e7ebe8e7f
1277120515 <1277120515@qq.com>
2018-06-18 21:51:57 +0800

    1

--- JRotate/src/jrotate/JRotate.java
+++ JRotate/src/jrotate/JRotate.java
@@ -100,7 +100,7 @@ public class JRotate
         return new double[]{qw, qx, qy, qz};
     }
     
-    //欧拉角 --> 四元数
+    //四元数 --> 欧拉角
     public static double[] Qua2Euler(double[] wxyz)
     {
         double qw, qx, qy, qz;
@@ -109,8 +109,51 @@ public class JRotate
         qy = wxyz[2];
         qz = wxyz[3];
 
-    }
+        double x, y, z;
+        x = Math.atan2(2 * (qw * qx + qy * qz), 1 - 2 * (qx * qx + qy * qy));
+        y = Math.asin(2 * (qw * qy - qx * qz));
+        z = Math.atan2(2 * (qw * qz + qx * qy), 1 - 2 * (qz * qz + qy * qy));
 
+        return new double[]{x, y, z};
+    }
     
+    //四元数 --> 旋转矩阵(矩阵x向量)
+    public static double[][] Qua2Mat(double[] wxyz)
+    {
+        double w, x, y, z;
+        w = wxyz[0];
+        x = wxyz[1];
+        y = wxyz[2];
+        z = wxyz[3];
+        double[][] mat = {
+            {w * w + x * x - y * y - z * z, 2 * (x * y - w * z), 2 * (w * y + x * z)},
+            {2 * (x * y + w * z), w * w - x * x + y * y - z * z, 2 * (y * z - w * x)},
+            {2 * (x * z - w * y), 2 * (y * z + w * x), w * w - x * x - y * y + z * z}};
+        return mat;
+    }
+    
+    //旋转矩阵(矩阵x向量) --> 四元数
+    public static double[] Mat2Qua(double[][] mat)
+    {
+        double w,x, y, z;
+        double r11, r12, r13, r21, r22, r23, r31, r32, r33;
+
+        r11 = mat[0][0];
+        r12 = mat[0][1];
+        r13 = mat[0][2];
+        r21 = mat[1][0];
+        r22 = mat[1][1];
+        r23 = mat[1][2];
+        r31 = mat[2][0];
+        r32 = mat[2][1];
+        r33 = mat[2][2];
+
+        w = 0.5 * Math.sqrt(1 + r11 + r22 + r33);
+        x = Math.signum(r32 - r23) * 0.5 * Math.sqrt(1 + r11 - r22 - r33);
+        y = Math.signum(r13 - r31) * 0.5 * Math.sqrt(1 - r11 + r22 - r33);
+        z = Math.signum(r21 - r12) * 0.5 * Math.sqrt(1 - r11 - r22 + r33);
+
+        return new double[]{w, x, y, z};
+    }
 
 }
