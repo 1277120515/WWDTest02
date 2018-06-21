81a098c7d17460656ae3c3c1317a6c9ed654971a
1277120515 <1277120515@qq.com>
2018-06-21 18:05:55 +0800

    Revert "1"
    This reverts commit a26bf95f4bb8cdfe25e7df2ce118de016d7da141.

--- WWDTest02/src/gov/nasa/worldwind/render/Cone.java
+++ WWDTest02/src/gov/nasa/worldwind/render/Cone.java
@@ -162,7 +162,6 @@ public class Cone extends RigidShape
         return this.faceCount;
     }
 
-    @Override
     public int getSubdivisions()
     {
         return this.subdivisions;
@@ -191,7 +190,6 @@ public class Cone extends RigidShape
      * @param dc        the current drawContext.
      * @param shapeData the current globe-specific shape data
      */
-    @Override
     protected void computeSubdivisions(DrawContext dc, ShapeData shapeData)
     {
         // test again possible subdivision values
@@ -263,7 +261,6 @@ public class Cone extends RigidShape
      *
      * @param shapeData this shape's current shape data.
      */
-    @Override
     protected void makeGeometry(ShapeData shapeData)
     {
         // attempt to retrieve a cached unit box with the same number of subdivisions
